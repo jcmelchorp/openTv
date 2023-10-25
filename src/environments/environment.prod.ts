56f853f662067592dbb72db76ab4191443d2959c
Julio C. Melchor P <jcmelchorp@gmail.com>
2023-10-24 22:21:02 -0600

    reset

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    projectId: 'freetv-opentv',
-    appId: '1:516893133993:web:92c6a153236e4c26a2adb8',
-    databaseURL: 'https://freetv-opentv-default-rtdb.firebaseio.com',
-    storageBucket: 'freetv-opentv.appspot.com',
-    apiKey: 'AIzaSyDN39fpsojZcH9_ye-ByCIJtDRQdDpXzAE',
-    authDomain: 'freetv-opentv.firebaseapp.com',
-    messagingSenderId: '516893133993',
-    measurementId: 'G-DN8GVRYH5W',
+    projectId: '',
+    appId: '',
+    databaseURL: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+    measurementId: '',
   },
 };
