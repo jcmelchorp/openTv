a7a88cc621b4f349f0bac49bbf24c70f8a54600a
Julio C. Melchor P <jcmelchorp@gmail.com>
2024-04-29 21:21:39 -0600

    last update

--- src/app/core/services/layout.service.ts
+++ src/app/core/services/layout.service.ts
@@ -43,9 +43,9 @@ export class LayoutService {
   onWindowScroll() {
     const currentScrollPos = window.pageYOffset;
     if (this.prevScrollpos > 200) {
-      this.matToolbar._elementRef.nativeElement.classList.add('sticky');
+      //this.matToolbar._elementRef.nativeElement.classList.add('sticky');
     } else {
-      this.matToolbar._elementRef.nativeElement.classList.remove('sticky');
+      //this.matToolbar._elementRef.nativeElement.classList.remove('sticky');
     }
     this.prevScrollpos = currentScrollPos;
   }
